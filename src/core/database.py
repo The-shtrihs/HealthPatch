7f687f6cf27349f5fd72c0b10aef7b078b2987d3
Alysseum17 <ultrasarsenal2006@gmail.com>
2026-03-11 21:14:59 +0200

    fix use get settings function in database.py

--- src/core/database.py
+++ src/core/database.py
@@ -2,8 +2,8 @@ from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from src.core.config import settings
-
+from src.core.config import get_settings
+settings = get_settings()
 engine = create_async_engine(settings.database_url, echo=True)
 
 async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
